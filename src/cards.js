6487160a1e9501caca421ced92bb176dc01b178f
Elvin Yeung <elvin.yeung@onstar.com>
2016-04-28 07:07:06 -0400

    initial project

---
+++ src/cards.js
@@ -0,0 +1,4 @@
+
+var cards = function(){
+
+};
---
+++ test/card.spec.js
@@ -0,0 +1,7 @@
+var expect = chai.expect;
+
+describe('test', function(){
+  it('one single lonely test case', function(){
+    expect(true).to.equal(true);
+  });
+});
